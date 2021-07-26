a30e18bbae1bfae54079ca4a694f6303a899a25e
sangnguyen-tul-0920 <70987304+sangnguyen-tul-0920@users.noreply.github.com>
2021-07-26 15:09:42 -0500

    Added files for task 1

---
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Int
+{
+    public static void divide(int a, int b)
+    {
+        int res = 0;
+        try
+        {
+            res = a / b;
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Cannot divide by zero");
+        }
+        finally
+        {
+            Console.WriteLine("{0} / {1} = {2}", a, b, res);
+        }
+    }
+}
