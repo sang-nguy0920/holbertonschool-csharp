ad6474bf33ace0ddfe972834036c980ef1248323
sangnguyen-tul-0920 <70987304+sangnguyen-tul-0920@users.noreply.github.com>
2021-07-27 12:58:48 -0500

    added fix files for task 2

--- 0x06-csharp-classes/2-enemy/2-enemy.cs
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -18,7 +18,7 @@ namespace Enemies
             {
                 throw new ArgumentException("Health must be greater than or equal to 0");
             }
-            health = 0;
+            health = value;
         }
 
     }
