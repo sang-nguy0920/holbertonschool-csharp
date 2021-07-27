c080bd10bd11f4755f7eac1490eeb5b9dfd1c2b8
sangnguyen-tul-0920 <70987304+sangnguyen-tul-0920@users.noreply.github.com>
2021-07-27 12:41:34 -0500

    added files for task 1

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>Initializes a new instance of the Zombie class.</summary>
+    class Zombie
+    {
+        public int health;
+        /// <summary>Public constructor for Zombie class.</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+    }
+}
