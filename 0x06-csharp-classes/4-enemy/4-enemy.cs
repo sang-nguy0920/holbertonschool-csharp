5553555b802456b4c2e09df4d5d02ca96c745dc1
sangnguyen-tul-0920 <70987304+sangnguyen-tul-0920@users.noreply.github.com>
2021-07-27 13:50:58 -0500

    added fix files for task 4

---
+++ 0x06-csharp-classes/4-enemy/4-enemy.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>Initializes a new instance of the Zombie class.</summary>
+    class Zombie
+    {
+        /// <summary>Private field for Zombie class.</summary>
+        private int health;
+
+        /// <summary>Public constructor for Zombie class.</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+        /// <summary>Public constructor for Zombie class.</summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            health = value;
+        }
+
+        /// <summary>Private field for Zombie class.</summary>
+        private string name = "(No name)";
+
+        /// <summary>Public constructor for Zombie class.</summary>        
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        /// <summary>Public constructor for Zombie class.</summary>
+        public int GetHealth()
+        {
+            return (health);
+        }
+
+    }
+}
