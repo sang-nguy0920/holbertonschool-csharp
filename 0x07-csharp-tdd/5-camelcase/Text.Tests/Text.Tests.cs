31218e9266208eb2a1717a6b7f6079df042c4990
sangnguyen-tul-0920 <70987304+sangnguyen-tul-0920@users.noreply.github.com>
2021-07-29 16:55:15 -0500

    added tests for task 5

--- 0x07-csharp-tdd/5-camelcase/Text.Tests/Text.Tests.cs
+++ 0x07-csharp-tdd/5-camelcase/Text.Tests/Text.Tests.cs
@@ -4,15 +4,36 @@ namespace Text.Tests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void one()
         {
+            string s = "this";
+            int count = Str.CamelCase(s);
+            Assert.AreEqual(1, count);
         }
 
         [Test]
-        public void Test1()
+        public void two()
         {
-            Assert.Pass();
+            string s = "this Is";
+            int count = Str.CamelCase(s);
+            Assert.AreEqual(2, count);
+        }
+
+        [Test]
+        public void multiple()
+        {
+            string s = "this Is Dumb";
+            int count = Str.CamelCase(s);
+            Assert.AreEqual(3, count);
+        }
+
+        [Test]
+        public void empty()
+        {
+            string s = "";
+            int count = Str.CamelCase(s);
+            Assert.AreEqual(0, count);
         }
     }
 }
--- 0x07-csharp-tdd/5-camelcase/Text/Text.cs
+++ 0x07-csharp-tdd/5-camelcase/Text/Text.cs
@@ -2,7 +2,23 @@
 
 namespace Text
 {
-    public class Class1
+    /// <summary>Public Str class.</summary>
+    public class Str
     {
+        /// <summary>Public CamelCase for Str class.</summary>
+        public static int CamelCase(string s)
+        {
+            if (s == "")
+            {
+                return 0;
+            }
+            int count = 1;
+            foreach (char word in s)
+            {
+                if (Char.IsUpper(word) == true)
+                count +=1;
+            }
+            return count;
+        }
     }
 }
