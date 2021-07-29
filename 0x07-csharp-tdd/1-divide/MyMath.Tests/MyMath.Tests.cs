40015d4ccf70fa1e762822032d8b49667e79aef5
sangnguyen-tul-0920 <70987304+sangnguyen-tul-0920@users.noreply.github.com>
2021-07-29 10:52:17 -0500

    added files for task 1

--- 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -2,17 +2,25 @@ using NUnit.Framework;
 
 namespace MyMath.Tests
 {
+    /// <summary>Public Tests class.</summary>
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void regular()
         {
+            int[,] matrix = new int[,] {{2, 3}, {4, 5}, {6, 7}};
+            int num = 1;
+            int[,] res = Matrix.Divide(matrix, num);
+            Assert.AreEqual(matrix, res);
         }
 
         [Test]
-        public void Test1()
+        public void zerodivide()
         {
-            Assert.Pass();
+            int[,] matrix = new int[,] {{2, 3}, {4, 5}, {6, 7}};
+            int num = 0;
+            int[,] res = Matrix.Divide(matrix, num);
+            Assert.AreEqual(null, res);
         }
     }
 }
--- 0x07-csharp-tdd/1-divide/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/1-divide/MyMath/MyMath.cs
@@ -2,7 +2,38 @@
 
 namespace MyMath
 {
-    public class Class1
+    /// <summary>Public Matrix class.</summary>
+    public class Matrix
     {
+        /// <summary>Public constructor for Matrix class.</summary>
+        public static int[,] Divide(int[,] matrix, int num)
+        {
+            if (matrix == null)
+            {
+                return null;
+            }
+
+            int [,] temp = matrix;
+            int row = temp.GetLength(0);
+            int col = temp.GetLength(1);
+
+            try
+            {
+                for (int x = 0; x < row; x++)
+                {
+                    for (int y = 0; y < col; y++)
+                    {
+                        temp[x, y] /= num;
+                    }
+                }
+
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Num cannot be 0");
+                return null;
+            }
+            return (temp);
+        }
     }
 }
