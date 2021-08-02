b1c9ee3dc0f3d6ba87f00531eaa2dcf4a9e7aff9
sangnguyen-tul-0920 <70987304+sangnguyen-tul-0920@users.noreply.github.com>
2021-08-02 11:24:34 -0500

    added solutions for task 0 and 1

---
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+/// <summary>Public Obj class.</summary>
+class Obj
+{
+    /// <summary>Public Obj class.</summary>
+    public static bool IsOfTypeInt(object obj)
+    {
+        return (obj is int);
+    }
+}
---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+/// <summary>Public Obj class.</summary>
+class Obj
+{
+    /// <summary>Public Obj class.</summary>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        return (typeof(Array).IsInstanceOfType(obj));
+    }
+}
