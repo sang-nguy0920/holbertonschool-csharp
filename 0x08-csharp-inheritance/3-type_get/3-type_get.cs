73d47e016798d032b7aeaa098fa42292aa235e07
sangnguyen-tul-0920 <70987304+sangnguyen-tul-0920@users.noreply.github.com>
2021-08-02 13:37:32 -0500

    added solution for task 3

--- 0x08-csharp-inheritance/3-type_get/3-type_get.cs
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
 
 
 /// <summary>Public Obj class.</summary>
@@ -7,6 +9,17 @@ class Obj
     /// <summary>Public Obj class.</summary>
     public static void Print(object myObj)
     {
+        TypeInfo details = myObj.GetType().GetTypeInfo();
+        Console.WriteLine("{0} Properties:", details.Name);
+        foreach (PropertyInfo p in details.GetProperties())
+        {
+            Console.WriteLine(p.Name);
+        }
+        Console.WriteLine("{0} Methods:", details.Name);
+        foreach (MethodInfo m in details.GetMethods())
+        {
+            Console.WriteLine(m.Name);
+        }
         
     }
 }
