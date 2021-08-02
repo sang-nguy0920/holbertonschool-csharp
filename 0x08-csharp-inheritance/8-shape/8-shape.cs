4d00fcff44c8203502527e5967feb03e5a3b5db9
sangnguyen-tul-0920 <70987304+sangnguyen-tul-0920@users.noreply.github.com>
2021-08-02 13:11:53 -0500

    added solution for task 8

---
+++ 0x08-csharp-inheritance/8-shape/8-shape.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+/// <summary>Base Shape class.</summary>
+class Shape
+{
+    /// <summary>Base Shape class.</summary>
+    public virtual int Area()
+    {
+        throw new System.NotImplementedException("Area() is not implemented");
+    }
+
+}
+
+/// <summary>Base Rectangle class.</summary>
+class Rectangle:Shape
+{
+    private int width;
+    private int height;
+
+    public int Width
+    {
+        get {return (width);}
+        set{
+            if (value < 0)
+            throw new System.ArgumentException("Width must be greater than or equal to 0");
+            else
+            width = value;
+        }
+    }
+
+    public int Height
+    {
+        get {return (height);}
+        set{
+            if (value < 0)
+            throw new System.ArgumentException("Height must be greater than or equal to 0");
+            else
+            height = value;
+        }
+    }
+
+    public new int Area()
+    {
+        return (width * height);
+    }
+
+    public override string ToString()
+    {
+        return String.Format("[Rectangle] {0} / {1}", width, height);
+    }
+}
+
+/// <summary>Square class.</summary>
+class Square:Rectangle
+{
+    private int size;
+    public int Size
+    {
+        get {return (size);}
+        set{
+            if (value < 0)
+            throw new System.ArgumentException("Size must be greater than or equal to 0");
+            else
+            size = value;
+            Height = value;
+            Width = value;
+        }
+    }
+
+}
