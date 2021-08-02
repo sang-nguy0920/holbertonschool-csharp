a32b6d976cebdc2ae6cba50db86a6c34e772d356
sangnguyen-tul-0920 <70987304+sangnguyen-tul-0920@users.noreply.github.com>
2021-08-02 12:45:29 -0500

    added solution for task 5

---
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+/// <summary>Base Shape class.</summary>
+class Shape
+{
+    /// <summary>Base Shape class.</summary>
+    public virtual int Area()
+    {
+        throw new System.NotImplementedException("Area() is not implemented");
+    }
+
+}
