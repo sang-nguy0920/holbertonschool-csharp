ee86c25ae04d5ef08b51538552ca322b4b8ef11d
sangnguyen-tul-0920 <70987304+sangnguyen-tul-0920@users.noreply.github.com>
2021-08-24 11:44:03 -0500

    added files for task 12

---
+++ 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>Public VectorMath class.</summary>
+class VectorMath
+{
+    /// <summary>Calculates dot product of either two 2D or two 3D vectors.</summary>
+    public static double DotProduct(double[] vector1, double[] vector2)
+    {
+        double result = -1;
+        if ((vector1.Length == 2 || vector1.Length == 3) && vector1.Length == vector2.Length)
+        {
+            result = 0;
+            for (int i = 0; i < vector1.Length; i++)
+            result += (vector1[i] * vector2[i]);
+        }
+        return (result);
+    }
+}
