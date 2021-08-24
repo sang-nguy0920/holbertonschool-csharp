6723184eaa8ac53a6a2a438e660e15356547a10b
sangnguyen-tul-0920 <70987304+sangnguyen-tul-0920@users.noreply.github.com>
2021-08-24 11:33:04 -0500

    added files for task 9

---
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>Public VectorMath class.</summary>
+class VectorMath
+{
+    /// <summary>Multiplies a vector/scalar, returns the resulting vector.</summary>
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        double[] res_vec = new double[1] {-1};
+        if (vector.Length == 2)
+        {
+            res_vec = new double[2] {vector[0] * scalar, vector[1] * scalar};
+        }
+        else if (vector.Length == 3)
+        {
+            res_vec = new double[3] {vector[0] * scalar, vector[1] * scalar, vector[2] * scalar};
+        }
+        return (res_vec);
+    }
+}
