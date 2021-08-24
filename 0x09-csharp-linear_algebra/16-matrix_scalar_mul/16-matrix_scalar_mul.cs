621d6d75a8f7629822ce8d1169f8d69d3b3c7e9c
sangnguyen-tul-0920 <70987304+sangnguyen-tul-0920@users.noreply.github.com>
2021-08-24 12:46:56 -0500

    added files for task 16

---
+++ 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>Public MatrixMath class.</summary>
+class MatrixMath
+{
+    /// <summary>Multiplies a matrix and a scalar and returns the resulting matrix.</summary>
+    public static double[,] MultiplyScalar(double[,] matrix, double scalar)
+    {
+        double[,] old = new double[,] {{-1}};
+        int height = matrix.GetLength(0);
+        int width = matrix.GetLength(1);
+        double[,] res;
+
+        if (height == 2 && width == 2)
+                res = new double[2, 2];
+        else if (height == 3 && width == 3)
+                res = new double[3, 3];
+        else
+            return (old);
+        for (int x = 0; x < height; x++)
+        {
+            for (int y = 0; y < width; y++)
+            {
+                res[x, y] = matrix[x, y] * scalar;
+            }
+        }
+        return (res);
+    }
+}
