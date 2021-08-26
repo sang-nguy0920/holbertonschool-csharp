3957ff34b0d0a8904a703938ba517b24e56da166
sangnguyen-tul-0920 <70987304+sangnguyen-tul-0920@users.noreply.github.com>
2021-08-26 12:23:35 -0500

    added files for task 30

---
+++ 0x09-csharp-linear_algebra/30-cross_product/30-cross_product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>Public VectorMath class.</summary>
+class VectorMath
+{
+    /// <summary>Calculates cross product of two 3D vectors.</summary>
+    public static double[] CrossProduct(double[] vector1, double[] vector2)
+    {
+        double[] vector3;
+
+        if (vector1.Length == 3 && vector2.Length == 3)
+        {
+            vector3 = new double[3] {
+                (vector1[1] * vector2[2]) - (vector1[2] * vector2[1]),
+                (vector1[2] * vector2[0]) - (vector1[0] * vector2[2]),
+                (vector1[0] * vector2[1]) - (vector1[1] * vector2[0])
+            };
+            return (vector3);
+        }
+        return (new double[] {-1});
+    }
+}
