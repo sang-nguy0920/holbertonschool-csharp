6b9c66207cd76ab969ed6d5954810ca07bf01cef
sangnguyen-tul-0920 <70987304+sangnguyen-tul-0920@users.noreply.github.com>
2021-08-26 11:53:40 -0500

    added files for task 25

---
+++ 0x09-csharp-linear_algebra/25-matrix_transpose/25-main.cs
@@ -0,0 +1,25 @@
+  
+using System;
+using System.Windows;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double[,] matrixOne2d =  { { 1, 2 }, { 3, 4 } };
+        double[,] res2d;
+
+        res2d = MatrixMath.Transpose(matrixOne2d);
+
+        for (int i = 0; i < res2d.GetLength(0); i++)
+        {
+            for (int j = 0; j < res2d.GetLength(1); j++)
+            {
+                Console.Write(res2d[i, j]);
+                if (j != res2d.GetLength(1) - 1)
+                    Console.Write(", ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
