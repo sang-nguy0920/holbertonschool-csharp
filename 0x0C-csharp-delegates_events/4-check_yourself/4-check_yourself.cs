f96f841683ae18d4b6a8f501eb545a39a5e02924
sangnguyen-tul-0920 <70987304+sangnguyen-tul-0920@users.noreply.github.com>
2021-10-28 17:59:34 -0500

    added files for task 4

---
+++ 0x0C-csharp-delegates_events/4-check_yourself/4-check_yourself.cs
@@ -0,0 +1,155 @@
+using System;
+
+///<summary> public class called Player </summary>
+class Player
+{
+    private string name { get; set; }
+    private float maxHp { get; set; }
+    private float hp { get; set; }
+    private string status { get; set; }
+    /// <summary> public EventHandler<CurrentHPArgs/> HPCheck </summary>
+    public EventHandler<CurrentHPArgs> HPCheck;
+
+    ///<summary>Constructor for Player class.</summary>
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        if (maxHp <= 0f)
+        {
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            this.maxHp = 100f;
+        }
+        else
+        {
+            this.maxHp = maxHp;
+        }
+        this.name = name;
+        this.hp = this.maxHp;
+        this.status = $"{this.name} is ready to go!";
+        HPCheck += CheckStatus;
+    }
+
+    ///<summary> PrintHealth() </summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{this.name} has {this.hp} / {this.maxHp} health");
+    }
+
+    /// <summary> for checker </summary>
+    delegate void CalculateHealth(float health);
+
+    /// <summary> for checker </summary>
+    public void TakeDamage(float damage)
+    {
+        if (damage < 0f)
+        {
+            damage = 0f;
+            Console.WriteLine($"{this.name} takes 0 damage!");
+        }
+        else
+        {
+            Console.WriteLine($"{this.name} takes {damage} damage!");
+        }
+        this.hp -= damage;
+        ValidateHP(this.hp);
+    }
+
+    /// <summary> for checker</summary>
+    public void HealDamage(float heal)
+    {
+        if (heal < 0f)
+        {
+            heal = 0f;
+            Console.WriteLine($"{this.name} heals 0 HP!");
+        }
+        else
+        {
+            Console.WriteLine($"{this.name} heals {heal} HP!");
+        }
+        this.hp +=heal;
+        ValidateHP(this.hp);
+    }
+
+    ///<summary> for checker</summary>
+    public void ValidateHP(float newHp)
+    {
+        if (newHp < 0f)
+        {
+            this.hp = 0f;
+        }
+        else if (newHp > this.maxHp)
+        {
+            this.hp = maxHp;
+        }
+        else
+        {
+            this.hp = newHp;
+        }
+        HPCheck(this, new CurrentHPArgs(this.hp));
+    }
+
+    ///<summary> for checker</summary>
+    public float ApplyModifier(float baseValue, Modifier modifier)
+    {
+        if (modifier == Modifier.Strong)
+        {
+            return baseValue * 1.5f;
+        }
+        else if (modifier == Modifier.Weak)
+        {
+            return baseValue / 2f;
+        }
+        else
+        {
+            return baseValue;
+        }
+    }
+
+        /// <summary> for checker</summary>
+    private void CheckStatus(object sender, CurrentHPArgs e)
+    {
+        if (e.currentHp == this.maxHp)
+        {
+            this.status = $"{this.name} is in perfect health!";
+        }
+        else if (e.currentHp >= (this.maxHp / 2))
+        {
+            this.status = $"{this.name} is doing well!";
+        }
+        else if (e.currentHp >= (this.maxHp / 4))
+        {
+            this.status = $"{this.name} isn't doing too great...";
+        }
+        else if (e.currentHp > 0)
+        {
+            this.status = $"{this.name} needs help!";
+        }
+        else
+        {
+            this.status = $"{this.name} is knocked out!";
+        }
+        Console.WriteLine(this.status);
+    }
+}
+
+/// <summary> for checker</summary>
+enum Modifier
+{
+    Weak,
+    Base,
+    Strong
+}
+/// <summary> for checker</summary>
+delegate float CalculateModifier(float baseValue, Modifier modifier);
+
+/// <summary> class CurrentHPArgs </summary>
+class CurrentHPArgs : EventArgs
+{
+    /// <summary> for checker</summary>
+       public readonly float currentHp;
+    
+    /// <summary> for checker</summary>
+    public CurrentHPArgs(float newHP)
+    {
+        this.currentHp = newHP;
+    }
+}
